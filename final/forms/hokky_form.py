8728e560f242b2238a4e30aa990a413333b53731
aqvi1a <glc500q@gmail.com>
2022-04-28 00:45:09 +0300

    a

--- final/forms/hokky_form.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms import BooleanField, SubmitField
-
-
-class HokkyForm(FlaskForm):
-    content = TextAreaField("Содержание")
-    submit = SubmitField('Применить')
